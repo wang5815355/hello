b7ca0821a684a1bfe923864a69c1597ceddaf916
wang5815355 <393867916@qq.com>
2013-08-26 14:57:15 +0800

    完成头像上传页面 2013826

--- Lib/Action/IndexAction.class.php
+++ Lib/Action/IndexAction.class.php
@@ -1,5 +1,5 @@
 <?php
-class IndexAction extends Action {
+class IndexAction extends GlobalAction {
     
 	/**
 	 * 首页显示方法
@@ -12,6 +12,43 @@ class IndexAction extends Action {
     	}else{//如果cookie中不存在用户账号则跳转到登录页面上
     		redirect('../Public/login');
     		$this->display();
+    	}
+    }
+    
+    public function doImageUp(){
+    	//获取当前登录用户email账号
+    	$email = $this->getUserName();
+    	 
+    	//获取分页类
+    	import('ORG.Net.UploadFile');
+    	$upload = new UploadFile();// 实例化上传类
+    	$upload->maxSize  = 3145728 ;// 设置附件上传大小
+    	$upload->allowExts  = array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+    	$upload->savePath =  './Public/Uploads/';// 设置附件上传目录
+    	$upload->thumb = true;
+    	$upload->thumbMaxWidth = '50,220';
+    	$upload->thumbMaxHeight = '50,200';
+    	$upload->thumbRemoveOrigin = true;
+    	 
+    	if(!$upload->upload()) {// 上传错误提示错误信息
+    		$infoMsg = $upload->getErrorMsg();
+    	}else{// 上传成功 获取上传文件信息
+    		$info =  $upload->getUploadFileInfo();
+    	}
+    	 
+    	// 保存表单数据 包括附件数据
+    	$userModel = M("User"); // 实例化User对象
+    	$userModel->create(); // 创建数据对象
+    	$map['email'] = $email;
+    	$data['faceimage'] = 'thumb_'.$info[0]['savename'];//缩略图文件名
+    	$result = $userModel->where($map)->save($data); // 写入用户数据到数据库
+    	 
+    	if($result != false){
+    		$infoMsg = '1';//数据保存成功
+    		echo "<script> parent.notice("."'".$infoMsg."'".");</script>";//执行提交页面的回调函数
+    	}else{
+    		$infoMsg = '2';//数据保存失败
+    		echo "<script> parent.notice("."'".$infoMsg."'".");</script>";
     	}
     }
     
--- Runtime/Cache/3bc6a2f0298f63af0d6b893a47c19b72.php
+++ Runtime/Cache/3bc6a2f0298f63af0d6b893a47c19b72.php
@@ -1,14 +1,107 @@
 <?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
 <html>
-  <head lang="en">
-    <title>Hello</title>
- 	<meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8">
+<head lang="en">
+<title>Hello</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8">
 <link type="text/css" href="__ROOT__/hello/Public/css/bootstrap-responsive.css" rel="stylesheet">
 <link type="text/css" href="__ROOT__/hello/Public/css/bootstrap.min.css" rel="stylesheet" media="screen">
 <script type="text/javascript" src="__ROOT__/hello/Public/js/jquery.js"></script>
 <script type="text/javascript" src="__ROOT__/hello/Public/js/bootstrap.min.js"></script>
-  </head>
-  <body>
-  	
-  </body>
+<style type="text/css">
+.faceimg {
+	width:180px;
+	height:180px;
+	margin: 190px auto 0;
+}
+
+p {
+	margin: 10px;
+	text-align: center;
+}
+
+#lable {
+	padding: 4px 10px 4px;
+	background: rgba(235, 235, 235, .5);
+	color: rgba(175, 175, 175, 0.9);
+	font-weight: 600;
+	text-shadow: 0 0px 0 rgba(0, 0, 0, 0);
+}
+.ifram{
+	visibility:hidden;
+}
+.fileSelect{
+	visibility:hidden;
+}
+.submit{
+	visibility:hidden;
+}
+.container{
+	overflow: hidden;
+	width:940px;
+}
+.slide1,.silde2{
+	width:940px;
+	height:500px;
+	float:left;
+}
+.silde2{
+	background: red;
+	height:700px;
+}
+.slides{
+	width:1880px;
+}
+</style>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('.thumbnail').click(function(){
+			$('.fileSelect').click();
+		});
+		
+		$('.fileSelect').change(function(){
+			$('.submit').click();
+		});
+		
+	});
+	
+	function notice(msg){
+		var m = msg;
+		if(m =="1"){
+			$(".slide1").animate({'opacity':'0.0'},200);
+			$(".slide1").animate({'margin-left':'-940px','opacity':'0.0',},2000);
+		}else{
+			alert('数据保存失败');
+		}
+	}
+</script>
+</head>
+
+<body style="">
+	<div class="container">
+		<div class="slides">
+			<!-- 首次登陆页面   -->
+			<div class="slide1">
+				<div class="faceimg">
+					<form enctype="multipart/form-data" 
+						action="__URL__/doImageUp" method="post"
+						target="ifram" id="formif">
+						<a href="#" class="thumbnail"> 
+							<img data-src="holder.js/160x160" alt="160x160" style="width: 176px; height: 160px;" src="__ROOT__/hello/Uploads/faceup.png">
+						</a>
+						<p>
+							<span class="label label-info" id="lable">点击图片上传真实头像</span>
+						</p>
+						<input type="file" name="photo" class="fileSelect"> 
+						<input type="submit" class="submit">
+					</form>
+					<iframe width="0px" height="0px" name="ifram" class="ifram"></iframe>
+				</div>
+			</div>
+	
+			<!-- 用户首页 -->
+			<div class="silde2"></div>
+		</div>
+	</div>
+	
+</body>
 </html>
