3ccd0ddd4b13584d4a0c0685f6807ddc09403572
wangkai <393867916@qq.com>
2013-08-27 23:08:53 +0800

    2013827 朋友表数据库设计

--- hello.sql
+++ hello.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2013 年 08 月 20 日 10:08
+-- 生成日期: 2013 年 08 月 27 日 14:53
 -- 服务器版本: 5.5.8
 -- PHP 版本: 5.3.3
 
@@ -12,6 +12,25 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- 数据库: `hello_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `hello_friend`
+--
+
+CREATE TABLE IF NOT EXISTS `hello_friend` (
+  `id` int(15) NOT NULL COMMENT '主键id',
+  `uemail` varchar(40) NOT NULL COMMENT '用户email账号',
+  `femail` varchar(40) NOT NULL COMMENT '好友email账号',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='好友关系表';
+
+--
+-- 转存表中的数据 `hello_friend`
+--
+
 
 -- --------------------------------------------------------
 
@@ -29,14 +48,14 @@ CREATE TABLE IF NOT EXISTS `hello_user` (
   `auth` char(1) NOT NULL DEFAULT '0' COMMENT '账号认证状态（3,未认证且非首次登陆0,未认证首次登陆1,已邮箱认证，且首次登陆。2,已认证且非首次登陆）',
   `regtime` datetime NOT NULL COMMENT '注册时间',
   PRIMARY KEY (`uid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='用户注册表' AUTO_INCREMENT=38 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='用户注册表' AUTO_INCREMENT=43 ;
 
 --
 -- 转存表中的数据 `hello_user`
 --
 
 INSERT INTO `hello_user` (`uid`, `uname`, `email`, `password`, `phonenumber`, `faceimage`, `auth`, `regtime`) VALUES
-(18, '王凯', '393867916@qq.com', '33a173a86a3c0bd6ff8a84f32432e6', '15820781327', NULL, '0', '0000-00-00 00:00:00'),
+(18, '王凯', '393867916@qq.com', '00fbfeb9353ca2055ddc129b2f0a01', '15820781327', 'thumb_521b5c1b4332d.jpg', '3', '0000-00-00 00:00:00'),
 (10, '王凯', '393817926@qq.com', 'wang5815355', '15820781327', NULL, '0', '0000-00-00 00:00:00'),
 (9, '王凯', '393867926@qq.com', 'wang5815355', '15820781327', NULL, '0', '0000-00-00 00:00:00'),
 (8, '王凯', '393867011@qq.com', 'wang5815355', '15820781327', NULL, '0', '0000-00-00 00:00:00'),
@@ -61,4 +80,9 @@ INSERT INTO `hello_user` (`uid`, `uname`, `email`, `password`, `phonenumber`, `f
 (34, '王凯', '111393867916@qq.com', '00fbfeb9353ca2055ddc129b2f0a01', '15820781327', NULL, '0', '0000-00-00 00:00:00'),
 (35, '王凯', '222393867916@qq.com', '33a173a86a3c0bd6ff8a84f32432e6', '15820781327', NULL, '0', '0000-00-00 00:00:00'),
 (36, '王凯', '555393867916@qq.com', '00fbfeb9353ca2055ddc129b2f0a01', '15820781327', NULL, '', '0000-00-00 00:00:00'),
-(37, '王凯', '555391233867916@qq.com', '33a173a86a3c0bd6ff8a84f32432e6', '15820781327', NULL, '1', '0000-00-00 00:00:00');
+(37, '王凯', '555391233867916@qq.com', '33a173a86a3c0bd6ff8a84f32432e6', '15820781327', NULL, '1', '0000-00-00 00:00:00'),
+(38, '王凯', '3938679886@qq.com', '00fbfeb9353ca2055ddc129b2f0a01', '15820781327', NULL, '0', '0000-00-00 00:00:00'),
+(39, '王凯', '39386755886@qq.com', '00fbfeb9353ca2055ddc129b2f0a01', '15820781327', NULL, '0', '0000-00-00 00:00:00'),
+(40, '王凯', '393864447916@qq.com', '00fbfeb9353ca2055ddc129b2f0a01', '15820781327', NULL, '0', '0000-00-00 00:00:00'),
+(41, '王凯', '393822264447916@qq.com', '33a173a86a3c0bd6ff8a84f32432e6', '15820781327', NULL, '0', '0000-00-00 00:00:00'),
+(42, '王凯', '3938222642447916@qq.com', '040bd08a4290267535cd247b8ba2ec', '15820781327', NULL, '0', '0000-00-00 00:00:00');
